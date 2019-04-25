16be72485e617096d82eab61479c00d99fe7412a
taylorda <49910858+taylorda@users.noreply.github.com>
2019-04-25 09:24:32 -0500

    Player Controller and Camera Controller

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public float dampTime = 0.2f;
+	public float screenEdgeBuffer = 4f;
+	public float minSize = 6.5f;
+	public Transform[] targets;
+
+	private Camera cam;
+	private Vector3 moveVelocity;
+	private float zoomSpeed;
+
+	void Awake() {
+		cam = GetComponentInChildren<Camera> ();
+	}
+
+	void FixedUpdate() {
+		Vector3 averagePosition = new Vector3 ();
+		int numTargets = 0;
+		for (int i = 0; i < targets.Length; i++) {
+			if (!targets [i].gameObject.activeSelf)
+				continue;
+			averagePosition += targets [i].position;
+			numTargets++;
+		}
+		if (numTargets > 1) {
+			averagePosition /= numTargets;
+		}
+		averagePosition.z = transform.position.z;
+
+		transform.position = Vector3.SmoothDamp (transform.position, averagePosition, ref moveVelocity, dampTime);
+
+		Vector3 desiredLocalPos = transform.InverseTransformPoint (averagePosition);
+
+		float size = 0f;
+
+		for (int i = 0; i < targets.Length; i++) {
+			if (!targets [i].gameObject.activeSelf)
+				continue;
+			Vector3 targetLocalPos = transform.InverseTransformPoint (targets [i].position);
+			Vector3 desiredPosToTarget = targetLocalPos - desiredLocalPos;
+			size = Mathf.Max (size, Mathf.Abs (desiredPosToTarget.z));
+			size = Mathf.Max (size, Mathf.Abs (desiredPosToTarget.y) / cam.aspect);
+		}
+
+		size += screenEdgeBuffer;
+		size = Mathf.Max (size, minSize);
+		cam.orthographicSize = Mathf.SmoothDamp (cam.orthographicSize, size, ref zoomSpeed, dampTime);
+	}
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour {
+	private Rigidbody2D rb;
+	public int playerID;
+	public float speed;
 
-	// Use this for initialization
-	void Start () {
-		
+	string horizontalMovementAxis;
+	string verticalMovementAxis;
+
+	void Start() {
+		rb = GetComponent<Rigidbody2D> ();
+		horizontalMovementAxis = "Horizontal" + playerID;
+		verticalMovementAxis = "Vertical" + playerID;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Update() {
+
+	}
+
+	void FixedUpdate() {
+		float hsp = Input.GetAxis (horizontalMovementAxis);
+		float vsp = Input.GetAxis (verticalMovementAxis);
+
+		Vector3 movement = new Vector3 (hsp, vsp, 0f);
+
+		transform.position += movement * speed;
 	}
 }
