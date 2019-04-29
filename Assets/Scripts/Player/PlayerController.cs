84bba7f10b9ffff9e58b14f8645f09c25287fb9c
yobloi <49910953+yobloi@users.noreply.github.com>
2019-04-29 09:25:07 -0500

    face mouse

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,18 +7,28 @@ public class PlayerController : MonoBehaviour {
 	private Rigidbody2D rb;
 	public int playerID;
 	public float speed;
+	public GameObject bullet;
+	public float targetSpeed;
+	public float bulletSpeed;
 
 	string horizontalMovementAxis;
 	string verticalMovementAxis;
+
+	string crosshairXAxis;
+	string crosshairYAxis;
+
+	Vector3 targetPosition = Vector3.zero;
 
 	void Start() {
 		rb = GetComponent<Rigidbody2D> ();
 		horizontalMovementAxis = "Horizontal" + playerID;
 		verticalMovementAxis = "Vertical" + playerID;
+		crosshairXAxis = "Crosshair X" + playerID;
+		crosshairYAxis = "Crosshair Y" + playerID;
 	}
 
 	void Update() {
-
+		FaceMouse ();
 	}
 
 	void FixedUpdate() {
@@ -28,5 +38,16 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = new Vector3 (hsp, vsp, 0f);
 
 		transform.position += movement * speed;
+
+
 	}
+
+	void FaceMouse(){
+		Vector3 mousePosition = Input.mousePosition;
+		mousePosition = Camera.main.ScreenToWorldPoint (mousePosition);
+		Vector3 called = transform.position - mousePosition;
+		float distance = called.magnitude;
+		Vector3 direction = called.normalized;
+	}
+
 }
