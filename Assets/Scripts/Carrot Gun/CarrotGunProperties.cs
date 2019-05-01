5cb0b47a39fe20dcc2015e839877fc2a76c8f46c
frasejj <49986345+frasejj@users.noreply.github.com>
2019-05-01 08:47:38 -0500

    Initial Commit

---
+++ Assets/Scripts/Carrot Gun/CarrotGunProperties.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarrotGunProperties : MonoBehaviour {
+
+
+    public Rigidbody2D projectile;
+    public Transform Spawnpoint;
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+
+            Rigidbody2D clone;
+            clone = (Rigidbody2D)Instantiate(projectile, Spawnpoint.position, projectile.rotation);
+
+        }
+
+	}
+}
